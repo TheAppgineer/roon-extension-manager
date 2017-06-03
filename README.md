ac3c0e6c8e845f6d419128b0e3a7c5a136c9670b
Jan Koudijs <theappgineer@gmail.com>
2017-06-03 19:28:46 +0200

    Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # roon-extension-manager
-Roon Extension to manage Roon Extensions
+
+Roon Extension for managing Roon Extensions.
+
+------------
+
+## Installation
+
+1. Install Node.js from https://nodejs.org.
+
+   * On Windows, install from the above link.
+   * On Mac OS, you can use [homebrew](http://brew.sh) to install Node.js.
+   * On Linux, you can use your distribution's package manager, but make sure it installs a recent Node.js. Otherwise just install from the above link.
+
+   Make sure you are running node 5.x or higher. This can be verified on the command line with the following command:
+
+   ```
+   node -v
+   ```
+
+   For example:
+
+   ```
+   $ node -v
+   v5.10.1
+   ```
+
+1. Install Git from https://git-scm.com/downloads.
+
+   * Following the instructions for the Operating System you are running.
+
+1. Setup an extension installation location via npm.
+
+   * Make a global extension directory:
+     ```
+     mkdir ~/.RoonExtensions
+     ```
+   * Configure npm to use the new directory path:
+     ```
+     npm config set prefix '~/.RoonExtensions'
+     ```
+
+   Note: If you use npm for other purposes as well, it may be handy to use an environment variable instead. Background information about setting up a global npm installation directory can be found [here](https://docs.npmjs.com/getting-started/fixing-npm-permissions#option-2-change-npms-default-directory-to-another-directory).
+
+1. Install the extension at the set location:
+    ```
+    npm install -g https://github.com/TheAppgineer/roon-extension-manager.git
+    ```
+
+1. Run it!
+    ```
+    cd ~/.RoonExtensions/lib/node_modules/roon-extension-manager
+    node .
+    ```
+
+    The extension should appear in Roon now. See Settings->Extensions. If you have multiple Roon Cores on the network, all of them should see it.
+
+## Notes
+* Remember that extensions are installed on the device at which the Extension Manager is running (hostname is included in the extension name to differentiate between multiple devices).
+* Automatic startup at system start is OS dependent and outside the scope of this document.
---
+++ manager.js
@@ -0,0 +1,300 @@
+// Copyright 2017 The Appgineer
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+"use strict";
+
+var RoonApi               = require("node-roon-api"),
+    RoonApiSettings       = require('node-roon-api-settings'),
+    RoonApiStatus         = require('node-roon-api-status'),
+    ApiExtensionInstaller = require('node-api-extension-installer');
+
+const ACTION_NO_CHANGE = 1;
+const ACTION_INSTALL = 2;
+const ACTION_UPDATE = 3;
+const ACTION_UNINSTALL = 4;
+const ACTION_START = 5;
+const ACTION_STOP = 6;
+
+var core;
+var pending_actions = {};
+
+var roon = new RoonApi({
+    extension_id:        'com.theappgineer.extension-manager',
+    display_name:        "Roon Extension Manager",
+    display_version:     "0.1.0",
+    publisher:           'The Appgineer',
+    email:               'theappgineer@gmail.com',
+    website:             'https://github.com/TheAppgineer/roon-extension-manager',
+
+    core_paired: function(core_) {
+        core = core_;
+        console.log(core.display_name);
+    },
+    core_unpaired: function(core_) {
+        core = undefined;
+    }
+});
+
+var ext_settings = roon.load_config("settings") || {
+    update_time: "02:00",
+};
+
+var svc_settings = new RoonApiSettings(roon, {
+    get_settings: function(cb) {
+        pending_actions = {};           // Start off with a clean list
+        cb(makelayout(ext_settings));
+    },
+    save_settings: function(req, isdryrun, settings) {
+        update_pending_actions(settings.values);
+
+        let l = makelayout(settings.values);
+        req.send_complete(l.has_error ? "NotValid" : "Success", { settings: l });
+
+        if (!isdryrun && !l.has_error) {
+            delete l.values.selected_extension;
+
+            ext_settings = l.values;
+            svc_settings.update_settings(l);
+            roon.save_config("settings", ext_settings);
+
+            perform_pending_actions();
+        }
+    }
+});
+
+var svc_status = new RoonApiStatus(roon);
+
+var installer = new ApiExtensionInstaller((installed) => {
+    console.log(installed);
+},
+(message, is_error) => {
+    svc_status.set_status(message, is_error);
+});
+
+roon.init_services({
+    provided_services: [ svc_settings, svc_status ]
+});
+
+function makelayout(settings) {
+    let l = {
+        values:    settings,
+        layout:    [],
+        has_error: false
+    };
+    let extension_list = installer.get_extension_list();
+
+    if (extension_list) {
+        let global = {
+            type:    "group",
+            title:   "[GLOBAL SETTINGS]",
+            items:   []
+        };
+        let selector = {
+            type:    "dropdown",
+            title:   "[EXTENSION]",
+            values:  [{ title: "(select extension)", value: undefined }],
+            setting: "selected_extension"
+        };
+        let extension = {
+            type:    "group",
+            title:   "(no extension selected)",
+            items:   [],
+        };
+        let status = {
+            type:    "label",
+        };
+        let action = {
+            type:    "dropdown",
+            title:   "Action",
+            values:  [{ title: "(select action)", value: undefined  }],
+            setting: "action"
+        }
+
+        selector.values = selector.values.concat(extension_list);
+
+        global.items.push({
+            type:    "string",
+            title:   "Check for updates @ [hh:mm]",
+            setting: "update_time"
+        });
+
+        let index = settings.selected_extension;
+        if (index != undefined) {
+            let details = installer.get_details(index);
+
+            if (details.description) {
+                extension.title = details.description;
+            } else {
+                extension.title = "(no description)";
+            }
+
+            const version = is_installed(index);
+            status.title = (version ? "INSTALLED: version " + version : "NOT INSTALLED")
+
+            if (is_pending(index)) {
+                action.values.push({ title: "Revert Action", value: ACTION_NO_CHANGE });
+            } else if (is_installed(index)) {
+                if (installer.has_update(index)) {
+                    action.values.push({ title: "Update", value: ACTION_UPDATE });
+                }
+                action.values.push({ title: "Uninstall", value: ACTION_UNINSTALL });
+                if (is_running(index)) {
+                    action.values.push({ title: "Stop", value: ACTION_STOP });
+                } else {
+                    action.values.push({ title: "Start", value: ACTION_START });
+                }
+            } else {
+                action.values.push({ title: "Install", value: ACTION_INSTALL });
+            }
+
+            extension.items.push({
+                type: "label",
+                title: "by: " + details.author
+            });
+            extension.items.push(status);
+            extension.items.push(action);
+        }
+
+        l.layout.push(global);
+        l.layout.push(selector);
+        l.layout.push(extension);
+
+        l.layout.push({
+            type:    "group",
+            title:   "[PENDING ACTIONS]",
+            items:   [{
+                type : "label",
+                title: get_pending_actions_string()
+            }]
+        });
+    } else {
+        l.layout.push({
+            type:    "label",
+            title:   "No repository found"
+        });
+    }
+
+    return l;
+}
+
+function is_installed(repos_index) {
+    return installer.get_status(repos_index).version;
+}
+
+function is_running(repos_index) {
+    return (installer.get_status(repos_index).state == 'running');
+}
+
+function is_pending(repos_index) {
+    for (let index in pending_actions) {
+        if (index == repos_index) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function update_pending_actions(settings) {
+    let repos_index = settings.selected_extension;
+    let action = settings.action;
+
+    if (action) {
+        if (action == ACTION_NO_CHANGE) {
+            // Remove action from pending_actions
+            delete pending_actions["" + repos_index];
+        } else {
+            let friendly;
+
+            // Update pending actions
+            switch (action) {
+                case ACTION_INSTALL:
+                    friendly = "Install ";
+                    break;
+                case ACTION_UPDATE:
+                    friendly = "Update ";
+                    break;
+                case ACTION_UNINSTALL:
+                    friendly = "Uninstall ";
+                    break;
+                case ACTION_START:
+                    friendly = "Start ";
+                    break;
+                case ACTION_STOP:
+                    friendly = "Stop ";
+                    break;
+            }
+
+            friendly += installer.get_details(repos_index).display_name;
+
+            let pending_action = {
+                action: action,
+                friendly: friendly
+            };
+
+            pending_actions["" + repos_index] = pending_action;
+        }
+
+        // Cleanup action
+        delete settings["action"];
+    }
+}
+
+function get_pending_actions_string() {
+    let pending_actions_string = ""
+
+    for (let repos_index in pending_actions) {
+        pending_actions_string += pending_actions[repos_index].friendly + "\n";
+    }
+
+    if (!pending_actions_string) {
+        pending_actions_string = "(none)";
+    }
+
+    return pending_actions_string;
+}
+
+function perform_pending_actions() {
+    for (let repos_index in pending_actions) {
+
+        switch (pending_actions[repos_index].action) {
+            case ACTION_INSTALL:
+                installer.install(+repos_index);
+                break;
+            case ACTION_UPDATE:
+                installer.update(+repos_index);
+                break;
+            case ACTION_UNINSTALL:
+                installer.uninstall(+repos_index);
+                break;
+            case ACTION_START:
+                installer.start(+repos_index);
+                break;
+            case ACTION_STOP:
+                installer.stop(+repos_index);
+                break;
+        }
+    }
+}
+
+function init() {
+    let os = require("os");
+    let hostname = os.hostname().split(".")[0];
+
+    roon.extension_reginfo.extension_id += "." + hostname;
+    roon.extension_reginfo.display_name += " @" + hostname;
+}
+
+init();
+roon.start_discovery();
